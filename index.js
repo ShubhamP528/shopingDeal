acdbe574e8dffc6b112b0bd11b83167f6711b95e
ShubhamP528 <shubham528prajapati@gmail.coom>
2024-04-19 19:14:56 +0530

    footer added

--- index.js
+++ index.js
@@ -1,52 +1,49 @@
 const express = require("express");
 const app = express();
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 // const Product=require('./model/product');
 // const seedDB=require('./seed');
-const path=require('path');
-const methodOverride=require('method-override');
+const path = require("path");
+const methodOverride = require("method-override");
 // const cookieParser=require('cookie-parser');
-const session=require('express-session');
-const flash=require('connect-flash');
-const localStrategy=require('passport-local');
-const passport=require('passport');
-const User=require('./model/user');
-
+const session = require("express-session");
+const flash = require("connect-flash");
+const localStrategy = require("passport-local");
+const passport = require("passport");
+const User = require("./model/user");
 
 // Routes
-const productRoute=require('./routes/product');
-const authRouter=require('./routes/auth');
-const cartRouter=require('./routes/cart');
+const productRoute = require("./routes/product");
+const authRouter = require("./routes/auth");
+const cartRouter = require("./routes/cart");
 
-
-if(process.env.NODE_ENV!=='production'){
-  require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch((err) => {
+    console.log("Connection ERROR!!");
+    console.log(err.message);
+  });
 
-mongoose.connect(process.env.DB_URL)
-.then(()=>{
-  console.log("DB connected");
-})
-.catch((err)=>{
-  console.log("Connection ERROR!!");
-  console.log(err.message);
-})
-
-
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname,"views"));
-
-
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-app.use(session({
-  secret: 'thisisnotagoodguy',
-  resave: false,
-  saveUninitialized: true,
-  // cookie: { secure: true }
-}));
+app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: "thisisnotagoodguy",
+    resave: false,
+    saveUninitialized: true,
+    // cookie: { secure: true }
+  })
+);
 // app.use(cookieParser());
 app.use(flash());
 
@@ -54,38 +51,34 @@ app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 // configuring the passport to use local strategy!!
 passport.use(new localStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req,res,next)=>{
-  res.locals.success=req.flash('success');
-  res.locals.error=req.flash('error');
-  res.locals.currUser=req.user;
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user;
   next();
-})
+});
 
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.render("products/start");
+});
 
-app.use(express.static('public'));
-app.get('/',(req,res)=>{
-  res.render('products/start');
-})
-
-app.get('/error',(req,res)=>{
-  res.render('error');
-})
+app.get("/error", (req, res) => {
+  res.render("error");
+});
 
 app.use(productRoute);
 app.use(authRouter);
 app.use(cartRouter);
 
-
-
 // seedDB();
 
-app.listen(process.env.PORT||3000, () => {
+app.listen(process.env.PORT || 3003, () => {
   console.log("server running at port 3000");
 });
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,7 @@
-const express=require('express');
-const router=express.Router();
-const User=require('../model/user');
-const passport = require('passport');
-
+const express = require("express");
+const router = express.Router();
+const User = require("../model/user");
+const passport = require("passport");
 
 // router.get('/fakeuser', async(req,res)=>{
 //     const user=new User({email:"shubham@gamil.com", username:"Shubham"});
@@ -10,81 +9,64 @@ const passport = require('passport');
 //     res.send(newUser);
 // })
 
-
 // registeration form
-router.get('/register',async(req,res)=>{
-    res.render('auth/signup');
-})
-
+router.get("/register", async (req, res) => {
+  res.render("auth/signup");
+});
 
 // registeration
-router.post('/register',async(req,res)=>{
-    try
-    {
-        const user=new User({email:req.body.email, userType:req.body.userType, username:req.body.username});
-        await User.register(user,req.body.password);
-        req.flash('success','Registered Successfully login and start' );
-        passport.authenticate('local',{
-            failureRedirect:'/register',
-            failureFlash:true,
-        });
-        res.redirect('/login'); 
-    }
-    catch(e)
-    {
-        req.flash('error',`${e.message}`);
-        res.redirect('/register');
-    }
-    
-})
-
+router.post("/register", async (req, res) => {
+  try {
+    const user = new User({
+      email: req.body.email.trim(),
+      userType: req.body.userType.trim(),
+      username: req.body.username.trim(),
+    });
+    await User.register(user, req.body.password.trim());
+    req.flash("success", "Registered Successfully login and start");
+    passport.authenticate("local", {
+      failureRedirect: "/register",
+      failureFlash: true,
+    });
+    res.redirect("/login");
+  } catch (e) {
+    req.flash("error", `${e.message}`);
+    res.redirect("/register");
+  }
+});
 
 // login form
-router.get('/login',async(req,res)=>{
-    res.render('auth/login');
-})
-
+router.get("/login", async (req, res) => {
+  res.render("auth/login");
+});
 
 // logined
-router.post('/login', passport.authenticate('local',{
-    failureRedirect:'/login',
-    failureFlash:true
-}),async(req,res)=>{
-    try
-    {
-        console.log(await User.find({username:"shubham"}));
-        req.flash('success',`Welcome Back!! ${req.user.username}`);
-        res.redirect('/product');
-    
-    }
-    catch(e)
-    {
-        req.flash('error',`${e.message}`)
-        res.redirect('/login')
-    }
-})
+router.post("/login", async (req, res) => {
+  try {
+    req.body.username = req.body.username.trim();
+    req.body.password = req.body.password.trim();
+    passport.authenticate("local", {
+      failureRedirect: "/login",
+      failureFlash: true,
+    });
+    req.flash("success", `Welcome Back!! ${req.body.username}`);
+    res.redirect("/product");
+  } catch (e) {
+    req.flash("error", `${e.message}`);
+    res.redirect("/login");
+  }
+});
 
+router.get("/logout", (req, res) => {
+  try {
+    req.logOut(() => {
+      req.flash("success", "logged out successfully");
+      res.redirect("/login");
+    });
+  } catch (e) {
+    req.flash("error", `${e.message}`);
+    res.redirect("/error");
+  }
+});
 
-router.get('/logout',(req,res)=>{
-    try
-    {
-        req.logOut(()=>{
-            req.flash('success','logged out successfully');
-            res.redirect('/login');
-        });
-        
-        
-    }
-    catch(e)
-    {
-        req.flash('error',`${e.message}`)
-        res.redirect('/error')
-    }
-    
-})
-
-
-
-
-
-module.exports=router;
+module.exports = router;
